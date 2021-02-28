c953cf8b1710f9e3568ab4906f99d02466e23bbf
Michiel Swaanen <michiel.swaanen.gsm@gmail.com>
2021-02-28 11:42:11 +0100

    :recycle: Created Preview component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import { Component } from "react";
-import DevicePreview from "./DevicePreview";
-import WebcamPreview from "./WebcamPreview";
 import WebcamMediaDevice from "./utils/WebcamMediaDevice";
 import MicrophoneMediaDevice from "./utils/MicrophoneMediaDevice";
-import RecordPreviewButton from "./RecordPreviewButton";
+import Preview from "./Preview";
 
 class App extends Component<any, any> {
 
   render() {
     const webcam: WebcamMediaDevice = new WebcamMediaDevice();
     const microphone: MicrophoneMediaDevice = new MicrophoneMediaDevice();
+
     webcam.select();
     microphone.select();
 
     return (
       <div>
-        <WebcamPreview webcam={ webcam }/>
-        <DevicePreview webcam={ webcam } microphone={ microphone }/>
-        <RecordPreviewButton webcam={ webcam } microphone={ microphone }/>
+        <Preview webcam={ webcam } microphone={ microphone }/>
       </div>
     )
   }
